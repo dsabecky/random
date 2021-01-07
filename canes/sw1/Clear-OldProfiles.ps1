9ece03b6d906081997665f66a070ee19b15fd08d
Dustin Sabecky <778367+dsabecky@users.noreply.github.com>
2021-01-07 11:41:52 -0500

    Removed empty scripts

--- canes/sw1/Clear-OldProfiles.ps1
+++
@@ -1,9 +0,0 @@
-#configure me
-$Exec = "\\dc01\software\Scripts\Clear-OldProfiles.ps1"
-
-function IsValidHost($Type) {
-    if ($Type -eq 'dc' -And $env:ComputerName -Match '^DC0[12]$') { return $True }
-    elseif ($Type -eq 'mts' -And $env:ComputerName -Match '^MTS(2|)$') { return $True }
-    elseif ($Type -eq 'ex' -And $env:ComputerName -Match '^EX0[12]$|IAEXET') { return $True }
-    else { return $False }
-}
