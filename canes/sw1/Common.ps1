ccf645faa9a07367dd00858d72ec799351d4e03c
Dustin Sabecky <778367+dsabecky@users.noreply.github.com>
2021-01-07 11:45:37 -0500

    psexec on remote machine

--- canes/sw1/Common.ps1
+++ canes/sw1/Common.ps1
@@ -9,3 +9,7 @@ if ($env:computername -NotMatch '^ex0[12]$') { exit 1 }
 
 # java settings file
 Set-Variable javaSettings "C:\Windows\Sun\Java\Deployment\deployment.properties"
+
+# psexec (with msi installer) on remote machine
+$Remote = read-host "Enter computer name"
+psexec \\$Remote -u $(whoami) -d -s cmd /c "msiexec /I "c:\full\path\here\.msi""
